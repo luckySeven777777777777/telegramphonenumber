1a51346ad9f8d270cf61fc7fcab9cd341d93c9d3
luckySeven777777777777 <lucky077779999@gmail.com>
2026-05-06 16:45:12 +0630

    Update login.py

--- login.py
+++ login.py
@@ -1,18 +1,19 @@
 from telethon import TelegramClient
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
+# 这里填你自己的 API 信息
+API_ID = 31295509
+API_HASH = "2d1e4c49213cc70e07afd487b18d71bc"
 
-phone = os.getenv("LOGIN_PHONE")  # 例如 17408999258（不加+）
-
-if not phone:
-    raise RuntimeError("请设置环境变量 LOGIN_PHONE")
+phone = "+12282629379"
 
 os.makedirs("sessions", exist_ok=True)
 
-client = TelegramClient(f"sessions/{phone}", API_ID, API_HASH)
-client.start(phone=phone)
+client = TelegramClient(f"sessions/{phone.replace('+', '')}", API_ID, API_HASH)
 
-print("✅ 登录成功，session 已生成")
-client.disconnect()
+async def main():
+    await client.start(phone=phone)
+    print("✅ 登录成功，session 已生成在 sessions 文件夹内")
+
+import asyncio
+asyncio.run(main())
